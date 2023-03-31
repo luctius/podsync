0a2359a6eabccf0e004a40cb9360678bd52014b6
Cor Peters <C.Peters@Eekels.com>
2023-03-31 15:23:30 +0200

    Hello Page

--- src/main.rs
+++ src/main.rs
@@ -71,6 +71,10 @@ async fn main() {
     let secure = args.secure();
     let podsync = Arc::new(PodSync::new(db));
 
+    let hello = warp::path::end()
+        .and(warp::get())
+        .map(|| "PodSync is Working!");
+
     let auth = {
         let login = warp::post()
             .and(warp::path!("api" / "2" / "auth" / String / "login.json"))
@@ -207,7 +211,8 @@ async fn main() {
         get.or(upload)
     };
 
-    let routes = auth
+    let routes = hello
+        .or(auth)
         .or(devices)
         .or(subscriptions)
         .or(episodes)
