bee52303db0eb7078a5420f992cde2184e041203
Cor Peters <C.Peters@Eekels.com>
2023-04-04 08:34:34 +0200

    added update.sh & added +x to scripts

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cargo clean
-git pull --rebase origin master &&  cross build --target armv7-unknown-linux-musleabihf --release --no-default-features --features "rustls" && scp target/armv7-unknown-linux-musleabihf/release/podsync nas:podsync/podsync.new && cargo clean
+./update.sh &&  cross build --target armv7-unknown-linux-musleabihf --release --no-default-features --features "rustls" && scp target/armv7-unknown-linux-musleabihf/release/podsync nas:podsync/podsync.new && cargo clean
