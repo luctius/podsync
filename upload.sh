d5eca0e2ef6fff0d167ff3a4b48c6b651b57c9cc
Cor Peters <C.Peters@Eekels.com>
2025-08-05 09:22:05 +0200

    use zigbuild

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cargo clean
-./update.sh && cargo zigbuild --target armv7-unknown-linux-gnueabihf.2.26 --release --no-default-features --features "rustls" &&  scp target/armv7-unknown-linux-musleabihf/release/podsync nas:podsync/podsync.new && cargo clean
+./update.sh && cargo zigbuild --target armv7-unknown-linux-gnueabihf.2.26 --release --no-default-features --features "rustls" &&  scp target/armv7-unknown-linux-gnueabihf/release/podsync nas:podsync/podsync.new && cargo clean
 
